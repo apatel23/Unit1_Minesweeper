70e583895dfafd5be41cfa3c977cce67f74aefb5
Alex Patel <apatel@mymail.mines.edu>
2015-09-22 10:04:23 -0600

    added basic functions to js file

--- cell.js
+++ cell.js
@@ -1,9 +1,35 @@
-<SCRIPT LANGUAGE="JavaScript">
+function beginGame() {
+    alert("Start the game!");
+    document.getElementById("end_button").disabled = false;
+    document.getElementById("start_button").disabled = true;
+}
 
-    document.getElementById("grid").innerHTML = 5 + 6;
-    function beginGame() {
-        alert("Starting Game!");
+function endGame() {
+    alert("End the game");   
+    document.getElementById("end_button").disabled = true;
+    document.getElementById("start_button").disabled = false;
+}
+
+
+var board_cell = {
+    cell_status : 0,
+    cell_visible : "true"
+};
+
+var x, y = 10;
+
+function createCells() {
+    var cells = new Array(x);
+    for (var i = 0; i < x; i++) {
+        cells[i] = new Array(y);   
     }
+}
+
+
+function drawBoard() {
     
-   
-</SCRIPT>
+}
+
+
+
+    
